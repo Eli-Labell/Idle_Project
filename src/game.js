099e9fec2df10a1553f28e61e03f53896b88cb9b
Kevin Humphreys <kmanh999@gmail.com>
2020-07-28 18:56:55 -0400

    fixed a goof

--- src/game.js
+++ src/game.js
@@ -5,34 +5,125 @@ var gameState = {
 	incomeUpgrades: {
 		socialDistancing: {
 			quantity: 1,
-			cost: 0,
-			income: 1
+			cost: 10,
+			income: 1,
+			friendlyName: "Social Distancing"
+		},
+		mask: {
+			quantity: 0,
+			cost: 50,
+			income: 5,
+			friendlyName: "Mask"
 		},
 		handSanitizer: {
-			quantity: 1,
-			cost: 100,
-			income: 10
+			quantity: 0,
+			cost: 500,
+			income: 10,
+			friendlyName: "Hand Sanitizer"
+		},
+		education: {
+			quantity: 0,
+			cost: 500,
+			income: 10,
+			friendlyName: "Education"
+		},
+		handWashing: {
+			quantity: 0,
+			cost: 500,
+			income: 10,
+			friendlyName: "Hand Washing"
+		},
+		wipes: {
+			quantity: 0,
+			cost: 500,
+			income: 10,
+			friendlyName: "Wipes"
+		},
+		vaccine: {
+			quantity: 0,
+			cost: 500,
+			income: 10,
+			friendlyName: "Vaccine"
+		},
+		testing: {
+			quantity: 0,
+			cost: 500,
+			income: 10,
+			friendlyName: "Testing"
 		}
 	}
 }
 
 function tick() {
 	for (let key of Object.keys(gameState.incomeUpgrades)) {
-		gameState.toiletPaper += gameState.incomeUpgrades[key].quantity * gameState.incomeUpgrades[key].income
+		gameState.toiletPaper += gameState.incomeUpgrades[key].quantity * gameState.incomeUpgrades[key].income;
 	}
 	gameState.tick++
-	showGameState()
-	console.log("tick")
+	if (gameState.tick % 10 == 0) {
+		// gameState.incomeUpgrades.handSanitizer.quantity++;
+	}
+	showGameState();
+	console.log("tick");
+	if (gameState.tick == TICK_ROLLOVER) {
+		gameState.tick = 0;
+	}
+}
+
+// Template for every buyable element in the inventory
+function buyableTemplate(key, buyable) {
+	return `<img class="buy pic" src="../img/sprites/${key}.png"/>
+			<ul class="buyable-stats">
+				<li>${buyable.friendlyName}</li>
+				<li>Cost: ${buyable.cost}</li>
+				<li>Quanity Owned: ${buyable.quantity}</li>
+			</ul>`
 }
 
 function showGameState() {
-	document.getElementById('score').innerHTML= `<span><h3>Rolls of toilet paper:</h3><h2>${gameState.toiletPaper}</h2></span>`
-    document.getElementById('ball').addEventListener("click", function () {
-        gameState.toiletPaper += 10
-    });
+	document.getElementById('score').innerHTML = `<span><h3>Rolls of toilet paper:</h3><h2>${gameState.toiletPaper}</h2></span>`
 }
 
+function populateInventory() {
+	let inventory = document.getElementById("inventory")
+	inventory.innerHTML = ""
+
+
+	for (let key of Object.keys(gameState.incomeUpgrades)) {
+		let buyable = document.createElement("div")
+		buyable.classList.add("buyable")
+		buyable.id = key
+		buyable.innerHTML = buyableTemplate(key, gameState.incomeUpgrades[key])
+		buyable.addEventListener("click", function () {
+			if (gameState.toiletPaper < gameState.incomeUpgrades[key].cost) {
+				console.log("false")
+				showGameState()
+				return false
+			}
+			gameState.toiletPaper -= gameState.incomeUpgrades[key].cost;
+			gameState.incomeUpgrades[key].quantity++
+			console.log("true")
+			showGameState()
+			updateInventory()
+			return true;
+		})
+		inventory.appendChild(buyable)
+	}
+}
+
+function updateInventory() {
+	let inventory = document.getElementById("inventory")
+	inventory.childNodes.forEach((element) => {
+		let key = element.id
+		console.log(`key: ${key}`)
+		element.innerHTML = buyableTemplate(key, gameState.incomeUpgrades[key])
+	})
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
-	showGameState()
-	setInterval(tick , 200);
+	document.getElementById('ball').addEventListener("click", function () {
+		gameState.toiletPaper += 10
+	});
+	populateInventory()
+	setInterval(tick, 200);
 }, false);
