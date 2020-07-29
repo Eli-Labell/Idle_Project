532bf59ba1ac23c4c01f03cc0f472bad562ad2a6
labelle <labelle@wit.edu>
2020-07-29 17:40:38 -0400

    Added Upgrade shelf and imgs for them. We are tracking the purchases of upgrades and multiplying points accordingly

--- src/game.js
+++ src/game.js
@@ -9,63 +9,110 @@ function initGameState() {
 		incomeUpgrades: { //Stores data about all items that can be purchased. You can buy as many of an item as you want
 			socialDistancing: {
 				quantity: 1,
-				cost: 10,
+				cost: 100,
 				income: 1,
 				multiplier: 1,
 				friendlyName: "Social Distancing"
 			},
 			mask: {
 				quantity: 0,
-				cost: 50,
+				cost: 500,
 				income: 5,
 				multiplier: 1,
 				friendlyName: "Mask"
 			},
 			handSanitizer: {
 				quantity: 0,
-				cost: 500,
+				cost: 2000,
 				income: 10,
 				multiplier: 1,
 				friendlyName: "Hand Sanitizer"
 			},
 			education: {
 				quantity: 0,
-				cost: 500,
+				cost: 50000,
 				income: 10,
 				multiplier: 1,
 				friendlyName: "Education"
 			},
 			handWashing: {
 				quantity: 0,
-				cost: 500,
+				cost: 100000,
 				income: 10,
 				multiplier: 1,
 				friendlyName: "Hand Washing"
 			},
 			wipes: {
 				quantity: 0,
-				cost: 500,
+				cost: 1000000,
 				income: 10,
 				multiplier: 1,
 				friendlyName: "Wipes"
 			},
 			vaccine: {
 				quantity: 0,
-				cost: 500,
+				cost: 5000000,
 				income: 10,
 				multiplier: 1,
 				friendlyName: "Vaccine"
 			},
 			testing: {
 				quantity: 0,
-				cost: 500,
+				cost: 50000000,
 				income: 10,
 				multiplier: 1,
 				friendlyName: "Testing"
 			}
 		},
 		multiplierUpgrades: { // Stores data about all multiplier upgrades that can be purchased. You can buy each upgrade once
-
+            socialDistancingUpgrade: {
+                purchased: 0,
+                cost: 1000,
+                multiplier: 2,
+                friendlyName: "Social Isolation"
+            },
+            maskUpgrade: {
+                purchased: 0,
+                cost: 50000,
+                multiplier: 2,
+                friendlyName: "N-95 Mask"
+            },
+            handSanitizerUpgrade: {
+                purchased: 0,
+                cost: 500000,
+                multiplier: 3,
+                friendlyName: "Hand Bleach"
+            },
+            educationUpgrade: {
+                purchased: 0,
+                cost: 1000000,
+                multiplier: 4,
+                friendlyName: "Master"
+            },
+            handWashingUpgrade: {
+                purchased: 0,
+                cost: 5000000,
+                multiplier: 5,
+                friendlyName: "Whole Arm Washing"
+            },
+            wipesUpgrade: {
+                purchased: 0,
+                cost: 50000000,
+                multiplier: 10,
+                friendlyName: "Disinfectant Towel"
+            },
+            vaccineUpgrade: {
+                purchased: 0,
+                cost: 10000000000,
+                multiplier: 20,
+                friendlyName: "Immunity"
+            },
+            testingUpgrade: {
+                purchased: 0,
+                cost: 5000000000000,
+                multiplier: 100,
+                friendlyName: "Testing"
+            }
 		}
 	}
 }
@@ -76,15 +123,19 @@ function setMultiplier(key, multiplier) {
 
 function tick() {
 	for (let key of Object.keys(gameState.incomeUpgrades)) {
-		console.log(gameState.incomeUpgrades[key]?.multiplier)
-		gameState.toiletPaper += gameState.incomeUpgrades[key]?.quantity * gameState.incomeUpgrades[key]?.income * gameState.incomeUpgrades[key]?.multiplier;
+        console.log(gameState.incomeUpgrades[key] ?.multiplier)
+        if (gameState.multiplierUpgrades[key + "Upgrade"].purchased === 1) {
+            gameState.toiletPaper += gameState.incomeUpgrades[key] ?.quantity * gameState.incomeUpgrades[key] ?.income * gameState.incomeUpgrades[key] ?.multiplier * gameState.multiplierUpgrades[key+"Upgrade"] ?.multiplier;
+        } else {
+            gameState.toiletPaper += gameState.incomeUpgrades[key] ?.quantity * gameState.incomeUpgrades[key] ?.income * gameState.incomeUpgrades[key] ?.multiplier;
+        }
 	}
 	gameState.tick++
 	if (gameState.tick % 10 == 0) {
 		// gameState.incomeUpgrades.handSanitizer.quantity++;
 	}
 	showGameState();
-	console.log("tick");
+	//console.log("tick");
 	if (gameState.tick == TICK_ROLLOVER) {
 		gameState.tick = 0;
 	}
@@ -96,7 +147,16 @@ function buyableTemplate(key, buyable) {
 			<ul class="buyable-stats">
 				<li>${buyable.friendlyName}</li>
 				<li>Cost: ${buyable.cost}</li>
-				<li>Quanity Owned: ${buyable.quantity}</li>
+				<li>Quantity Owned: ${buyable.quantity}</li>
+			</ul>`
+}
+
+function upgradableTemplate(key, upgradable) {
+    return `<img class="buy pic ${upgradable.purchased === 0 ? "grey" : ""}" src="../img/sprites/${key}.png"/>
+			<ul class="buyable-stats">
+				<li>${upgradable.friendlyName}</li>
+				<li>Cost: ${upgradable.cost}</li>
+				<li>Purchased: ${upgradable.purchased === 0 ? "False" : "True" }</li>
 			</ul>`
 }
 
@@ -131,6 +191,41 @@ function populateInventory() {
 	}
 }
 
+function populateItemShelf() {
+    let shelf = document.getElementById("shelf")
+    shelf.innerHTML = ""
+
+    for (let key of Object.keys(gameState.multiplierUpgrades)) {
+        let upgradable = document.createElement("div")
+        upgradable.classList.add("buyable")
+        upgradable.id = key
+        upgradable.innerHTML = upgradableTemplate(key, gameState.multiplierUpgrades[key])
+
+        upgradable.addEventListener("click", function () {
+            if (gameState.toiletPaper < gameState.multiplierUpgrades[key].cost) {
+                console.log("false")
+                showGameState()
+                return false
+            }
+
+            if (gameState.multiplierUpgrades[key].purchased != 0) {
+                console.log("false")
+                showGameState()
+                return false
+            }
+
+            gameState.toiletPaper -= gameState.multiplierUpgrades[key].cost;
+            gameState.multiplierUpgrades[key].purchased++
+            console.log(true)
+            showGameState()
+            updateShelf()
+            return true
+        })
+
+        shelf.appendChild(upgradable)
+    }
+}
+
 function updateInventory() {
 	let inventory = document.getElementById("inventory")
 	inventory.childNodes.forEach((element) => {
@@ -140,13 +235,24 @@ function updateInventory() {
 	})
 }
 
+function updateShelf(id) {
+    let shelf = document.getElementById("shelf")
+    console.log(shelf)
+    shelf.childNodes.forEach((element) => {
+        let key = element.id
+        console.log(key)
+        element.innerHTML =  upgradableTemplate(key, gameState.multiplierUpgrades[key])
+    })
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
 	retrieveGameState()
 	document.getElementById('ball').addEventListener("click", function () {
 		gameState.toiletPaper += 10
 	});
-	populateInventory()
+    populateInventory()
+    populateItemShelf()
 	setInterval(tick, 200);
 }, false);
 
